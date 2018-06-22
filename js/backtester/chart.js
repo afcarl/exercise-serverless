032a7918e5c2c7ed23c33195f357c366b7cca350
Shyrwinsteel Sia <shyrwin69@gmail.com>
2018-06-22 18:24:36 +0800

    Added new code

--- js/backtester/chart.js
+++ js/backtester/chart.js
@@ -5,109 +5,14 @@ $( document ).ready(function() {
       // create DOM
 
       $("#container").append("<div id='" + response[i].id + "' style='height: 900px; min-width: 310px'>");
-      createOHLC(response[i].id, response[i].name, response[i].ohlc, response[i].close)
+      createOHLC(response[i].id, response[i].name, response[i].ohlc, response[i].close, response[i].ac)
     }
   })
 
 })
 
-// EMA for closing prices
-function ema(data, days) {
-  // variables for EMA
-  var k = 2 / (days + 1)
-  var emaToday = 0
-  var emaYesterday = 0
 
-  var ema = []
-  for (var i = 0, len = data.length; i < len; i++) {
-    // calculate EMA(20)
-     if (i >= days) {
-       emaYesterday = (emaYesterday == null) ? 0 : emaYesterday
-       emaToday = data[i][4] * k + (emaYesterday * (1 - k))
-     } else {
-       emaToday = null
-     }
-
-     if(emaToday) {
-       ema.push([
-         data[i][0], // the date
-         emaToday
-       ])
-     }
-
-     emaYesterday = emaToday // set to today's ema
-  }
-  return ema
-}
-
-// series1 should be the long
-function crossover(seriesLong, seriesShort, seriesOffsetLong, seriesOffsetShort) {
-  var cs = []
-  var offsetDiff = seriesOffsetLong - seriesOffsetShort
-  for (var i = 1, len = seriesLong.length; i < len; i++) {
-        if(seriesShort[offsetDiff + i][1] > seriesLong[i][1] &&
-        seriesShort[offsetDiff + i - 1][1] < seriesLong[i - 1][1]) {
-          cs.push({
-            x: seriesLong[i][0], // the date
-            title: "Buy"
-          })
-        }
-
-        if(seriesShort[offsetDiff + i][1] < seriesLong[i][1] &&
-        seriesShort[offsetDiff + i - 1][1] > seriesLong[i - 1][1]) {
-          cs.push({
-            x: seriesLong[i][0], // the date
-            title: "Sell"
-          })
-        }
-  }
-
-  return cs
-}
-
-
-function evaluate(series, triggers) {
-  var startCash = 10000
-  var currentCash = startCash
-  var holdingPosition = false
-  var sellCommission = (0.795/100)
-  var buyCommission = (0.295/100)
-  var buyAdjustedPrice = 0
-  var sellAdjustedPrice = 0
-  var profitFromTrade = 0
-  var totalProfitFromTrades = 0
-  var numOfTrades = 0
-
-  for (var i = 0, len = series.length; i < len; i++) {
-      for (var j = 0, lenj = triggers.length; j < lenj; j++) {
-        if(series[i][0] == triggers[j].x){
-          if(triggers[j].title === "Buy" && !holdingPosition && i < series.length - 1) {
-            buyAdjustedPrice = series[i+1][1] + (series[i+1][1] * buyCommission)
-            holdingPosition = true
-            // console.log("Buy: " + buyAdjustedPrice);
-          }
-          if(triggers[j].title === "Sell" && holdingPosition && i < series.length - 1) {
-            sellAdjustedPrice = series[i+1][1] - (series[i+1][1] * sellCommission)
-            holdingPosition = false
-            profitFromTrade = sellAdjustedPrice - buyAdjustedPrice
-            currentCash = currentCash + profitFromTrade
-            totalProfitFromTrades = totalProfitFromTrades + profitFromTrade
-            numOfTrades += 1
-            // console.log("Sell: " + sellAdjustedPrice);
-            // console.log("Profit: " + profitFromTrade);
-          }
-        }
-      }
-  }
-
-  console.log("Overall Profit: " + (currentCash - startCash));
-  console.log("Number of Trades: " + numOfTrades);
-  console.log("Average Profit per Trade: " + (totalProfitFromTrades/numOfTrades));
-  console.log("Growth: " + (((currentCash - startCash) / startCash) * 100));
-
-}
-
-function createOHLC(id, title, ohlc, close) {
+function createOHLC(id, title, ohlc, close, ac) {
   var lastDate = ohlc[ohlc.length - 1][0],  // Get year of last data point
   days = 24 * 36e5;
    // create the chart
@@ -132,7 +37,7 @@ function createOHLC(id, title, ohlc, close) {
       title: {
         text: 'OHLC'
       },
-      height: '50%',
+      height: '30%',
       lineWidth: 0.2
     }, {
       labels: {
@@ -142,8 +47,19 @@ function createOHLC(id, title, ohlc, close) {
       title: {
         text: 'Close'
       },
-      top: '50%%',
-      height: '50%',
+      top: '30%%',
+      height: '30%',
+      offset: 0,
+    }, {
+      labels: {
+        align: 'right',
+        x: -3
+      },
+      title: {
+        text: 'Accelerator Oscillator'
+      },
+      top: '30%%',
+      height: '30%',
       offset: 0,
     }],
 
@@ -160,6 +76,13 @@ function createOHLC(id, title, ohlc, close) {
         data: close,
         yAxis: 1,
         color: "#666"
+      },
+      {
+        type: 'column',
+        name: 'AC',
+        data: ac,
+        yAxis: 2,
+        color: "#000088"
       }
     ],
 
@@ -180,4 +103,101 @@ function createOHLC(id, title, ohlc, close) {
      }
 
    })
+ }
+
+
+ // EMA for closing prices
+ function ema(data, days) {
+   // variables for EMA
+   var k = 2 / (days + 1)
+   var emaToday = 0
+   var emaYesterday = 0
+
+   var ema = []
+   for (var i = 0, len = data.length; i < len; i++) {
+     // calculate EMA(20)
+      if (i >= days) {
+        emaYesterday = (emaYesterday == null) ? 0 : emaYesterday
+        emaToday = data[i][4] * k + (emaYesterday * (1 - k))
+      } else {
+        emaToday = null
+      }
+
+      if(emaToday) {
+        ema.push([
+          data[i][0], // the date
+          emaToday
+        ])
+      }
+
+      emaYesterday = emaToday // set to today's ema
+   }
+   return ema
+ }
+
+ // series1 should be the long
+ function crossover(seriesLong, seriesShort, seriesOffsetLong, seriesOffsetShort) {
+   var cs = []
+   var offsetDiff = seriesOffsetLong - seriesOffsetShort
+   for (var i = 1, len = seriesLong.length; i < len; i++) {
+         if(seriesShort[offsetDiff + i][1] > seriesLong[i][1] &&
+         seriesShort[offsetDiff + i - 1][1] < seriesLong[i - 1][1]) {
+           cs.push({
+             x: seriesLong[i][0], // the date
+             title: "Buy"
+           })
+         }
+
+         if(seriesShort[offsetDiff + i][1] < seriesLong[i][1] &&
+         seriesShort[offsetDiff + i - 1][1] > seriesLong[i - 1][1]) {
+           cs.push({
+             x: seriesLong[i][0], // the date
+             title: "Sell"
+           })
+         }
+   }
+
+   return cs
+ }
+
+
+ function evaluate(series, triggers) {
+   var startCash = 10000
+   var currentCash = startCash
+   var holdingPosition = false
+   var sellCommission = (0.795/100)
+   var buyCommission = (0.295/100)
+   var buyAdjustedPrice = 0
+   var sellAdjustedPrice = 0
+   var profitFromTrade = 0
+   var totalProfitFromTrades = 0
+   var numOfTrades = 0
+
+   for (var i = 0, len = series.length; i < len; i++) {
+       for (var j = 0, lenj = triggers.length; j < lenj; j++) {
+         if(series[i][0] == triggers[j].x){
+           if(triggers[j].title === "Buy" && !holdingPosition && i < series.length - 1) {
+             buyAdjustedPrice = series[i+1][1] + (series[i+1][1] * buyCommission)
+             holdingPosition = true
+             // console.log("Buy: " + buyAdjustedPrice);
+           }
+           if(triggers[j].title === "Sell" && holdingPosition && i < series.length - 1) {
+             sellAdjustedPrice = series[i+1][1] - (series[i+1][1] * sellCommission)
+             holdingPosition = false
+             profitFromTrade = sellAdjustedPrice - buyAdjustedPrice
+             currentCash = currentCash + profitFromTrade
+             totalProfitFromTrades = totalProfitFromTrades + profitFromTrade
+             numOfTrades += 1
+             // console.log("Sell: " + sellAdjustedPrice);
+             // console.log("Profit: " + profitFromTrade);
+           }
+         }
+       }
+   }
+
+   console.log("Overall Profit: " + (currentCash - startCash));
+   console.log("Number of Trades: " + numOfTrades);
+   console.log("Average Profit per Trade: " + (totalProfitFromTrades/numOfTrades));
+   console.log("Growth: " + (((currentCash - startCash) / startCash) * 100));
+
  }
--- python/main.py
+++ python/main.py
@@ -1,9 +1,12 @@
 import client
 import indicators
+import utils
+import pandas as pd
+import numpy as np
 
 final_string = "["
-stocks = ["TEL"]#
-company_name = ["TEL"] #,"AEV","AGI","ALI","AP","BDO","BPI","DMC","EDC","EMP","FGEN","GLO","GTCAP","ICT","JFC","LTG","MBT","MEG","MER","MPI","PCOR","RLC","SCC","SECB","SM","SMC","SMPH","TEL","URC"]
+stocks = ["GLO"]#
+company_name = ["GLO"] #,"AEV","AGI","ALI","AP","BDO","BPI","DMC","EDC","EMP","FGEN","GLO","GTCAP","ICT","JFC","LTG","MBT","MEG","MER","MPI","PCOR","RLC","SCC","SECB","SM","SMC","SMPH","TEL","URC"]
 
 
 for i, stock in enumerate(stocks):
@@ -11,14 +14,19 @@ for i, stock in enumerate(stocks):
     dd = indicators.ema_close(dd)
 
     dd["date"] = dd.date.apply(lambda x: x * 1000) # convert timestamp
+    dd = indicators.ac(dd)
+    dd = utils.get_signals(dd)
+    dd['signal_buy'] = np.where(dd['signal'] == 1, "Buy", 0)
+    dd['signal_sell'] = np.where(dd['signal'] == -1, "Sell", 0)
 
     stock_id = "\"id\": \"{0}\"".format(stock)
     stock_name = "\"name\": \"{0}\"".format(company_name[i])
-    stock_data =  "\"ohlc\": " + dd.to_json(orient='values')
+    stock_data =  "\"ohlc\": " + dd[["date","open","high","low","close"]].to_json(orient='values')
     stock_combined = "{ " + stock_id + ", " + stock_name + ", " + stock_data + ","
-    stock_close = "\"close\": " + dd[["date","ema_close"]].to_json(orient='values') + "}"
-
-    final_string = final_string + stock_combined + stock_close
+    stock_close = "\"close\": " + dd[["date","ema_close"]].to_json(orient='values') + ","
+    indicator_ac = "\"ac\": " + dd[["date","ac"]].to_json(orient='values') # + ","
+    # signal = "\"signal\": " + dd[["date","signal_buy"]].to_json(orient='values')
+    final_string = final_string + stock_combined + stock_close + indicator_ac + "}"
 
     if i < len(stocks) - 1:
         final_string = final_string + ", "
