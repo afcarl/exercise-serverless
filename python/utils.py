7fe9e733953d0ebf9d85cd3171664f0b186ce4dd
shyrwinsia <shyrwinsia@gmail.com>
2018-08-25 02:07:55 +0800

    Made changes to the filter

--- python/utils.py
+++ python/utils.py
@@ -6,15 +6,26 @@ import client
 # get buy signals
 def get_signals(df):
     # start at 2 cause we need at least 2 days prior to test
-    for i in range(2, df.shape[0]):
-        ac = df.loc[i-2:i,"ac"].values # ac 2 days ago to now
-        if ac[0] < 0 and ac[1] < 0 and ac[0] < ac[1] and ac[1] < ac[2] and ac[2] > 0:
+    # for i in range(2, df.shape[0]):
+    #     ac = df.loc[i-2:i,"ac"].values # ac 2 days ago to now
+    #     if ac[0] < 0 and ac[1] < 0 and ac[0] < ac[1] and ac[1] < ac[2] and ac[2] > 0:
+    #         df.loc[i,"signal"] = 1
+    #     elif ac[0] > 0 and ac[1] > 0 and ac[0] > ac[1] and ac[1] > ac[2] and ac[2] < 0:
+    #         df.loc[i,"signal"] = -1
+    #     else:
+    #         df.loc[i,"signal"] = 0
+    #     i += 1
+    # df = df.fillna(0)
+    # return df
+
+    for i in range(1, df.shape[0]):
+        # if its green, and opens higher than the previous high, and the body is longer than the top wick
+        if df.loc[i,"open"] < df.loc[i,"close"] and \
+            df.loc[i-1,"high"] <= df.loc[i,"open"] and \
+            df.loc[i,"high"]/df.loc[i,"close"] < df.loc[i,"close"]/df.loc[i,"open"]:
             df.loc[i,"signal"] = 1
-        elif ac[0] > 0 and ac[1] > 0 and ac[0] > ac[1] and ac[1] > ac[2] and ac[2] < 0:
-            df.loc[i,"signal"] = -1
         else:
             df.loc[i,"signal"] = 0
-        i += 1
     df = df.fillna(0)
     return df
 
